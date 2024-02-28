09b15a150329fadef3957e160fb1950991a2dd3f
SqeekyBassoon <156135956+SqeekyBassoon@users.noreply.github.com>
2024-02-27 16:46:26 -0800

    files made on feb 27th

---
+++ HW5/script.js
@@ -0,0 +1,29 @@
+/*
+@file - script.js
+@author - Carter Luka
+
+    Contains the javascript for a basic currency conversion site that allows you to convert:
+        - USD to EUR, GBP, and Yen
+        - EUR to GPB, USD, and Yen
+        - Yen to GBP, USD, and EUR
+*/
+
+    const dollar = 1.0;
+    const pound = .79;
+    const euro = .92;
+    const yen = 150.49;
+
+/*
+Converts the value of currency1 to currency2
+*/
+function convertCurrency(currency1, currency1Type, currency2, currency2Type) {
+
+    
+
+}
+
+window.onload = function() {
+
+
+
+}
